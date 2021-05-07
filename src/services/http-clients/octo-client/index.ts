46b0981d2e368806cee6447edb9bc5dc5e2f018d
MHMALEK <mhos.malek@gmail.com>
2021-05-07 15:26:20 +0200

    fix 401

--- src/services/http-clients/octo-client/index.ts
+++ src/services/http-clients/octo-client/index.ts
@@ -1,16 +1,15 @@
 import { Octokit } from '@octokit/core';
 
 const generateNewClient: (payload?: any) => Octokit = ({ githubToken }) => {
-  const octokit = new Octokit({
-    auth: githubToken,
-  });
+  const octokit = new Octokit();
   return octokit;
 };
 
-const GithubClient = generateNewClient({
-  // process.env.REACT_APP_SAMPLE_PERSONAL_TOKEN 
-  // TODO: it's not a production app so I just added token like this
-  githubToken: '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',
-});
+const GithubClient = generateNewClient();
+// {
+// process.env.REACT_APP_SAMPLE_PERSONAL_TOKEN
+// TODO: it's not a production app so I just added token like this
+// githubToken: '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',
+// }
 
 export default GithubClient;
